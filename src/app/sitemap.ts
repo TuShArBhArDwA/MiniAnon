e89e5b9f15d2df34161e0b35bbf64cf738824ae6
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2026-05-26 02:55:20 +0530

    feat: add sitemap for Google Search Console

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://minianon.in'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/blogs`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+  ]
+}
